dfda35b8127eb0d9bf899ebe1ee62df10a4453cc
Dmitry <teramisu01@gmail.com>
2020-03-16 01:34:18 +0300

    feat: add activating phone screens

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ for (let i = 0; i < navLinks.length; i++) {
         navLinks[i].classList.add('active-link');
     }
 }
+
+// Activating phone screens
+
+let firstPhoneBtn = document.querySelectorAll('.phone-btn')[0];
+let secondPhoneBtn = document.querySelectorAll('.phone-btn')[1];
+let verticalPhoneDisplay = document.querySelectorAll('.black-display')[0];
+let horizontalPhoneDisplay = document.querySelectorAll('.black-display')[1];
+
+firstPhoneBtn.onclick = () => {
+    if(verticalPhoneDisplay.style.zIndex == 1){
+        verticalPhoneDisplay.style.zIndex = 0;
+    }
+    else{
+        verticalPhoneDisplay.style.zIndex = 1;
+    }
+}
+
+secondPhoneBtn.onclick = () => {
+    if(horizontalPhoneDisplay.style.zIndex == 1){
+        horizontalPhoneDisplay.style.zIndex = 0;
+    }
+    else{
+        horizontalPhoneDisplay.style.zIndex = 1;
+    }
+}
